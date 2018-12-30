05456aa600dbff05901493774078b070205037c3
Abhishek Mallik <dracovoldy@gmail.com>
2018-12-30 19:24:42 +0530

    added node server

---
+++ index.js
@@ -0,0 +1,14 @@
+var path = require('path'), express = require('express');
+var qs = require('querystring');
+var app = express();
+app.use('/ui5', express.static(path.join(__dirname, 'webapp')));
+
+app.get('/', function(req, res){
+	console.log("method in get/: " + req.method);    
+   	res.send("It works!");
+});
+
+
+app.listen(process.env.PORT || 3000, function(){
+     console.log("connect to localhost:3000");
+});
