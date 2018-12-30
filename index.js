599477a8a8f65b387b8f5edfa6680b76c1133361
Abhishek Mallik <dracovoldy@gmail.com>
2018-12-31 00:26:10 +0530

    favicon

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var path = require('path'), express = require('express');
 var qs = require('querystring');
 var app = express();
 app.use('/ui5', express.static(path.join(__dirname, 'webapp')));
+app.use(express.favicon());
 
 app.get('/', function(req, res){
 	console.log("method in get/: " + req.method);    
